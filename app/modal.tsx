d51785a033606e7a7a00e1765f83c42206062745
kendev101001 <ih2d2bf@gmail.com>
2025-11-17 17:59:21 +0000

    Removed old code

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,4 +1,4 @@
-// START of MODEL_A solution ------------------------------------------------------------------------------------------------------------------------
+
 import { API_URL } from '@/config/api';
 import { useAuth } from '@/contexts/AuthContext';
 import { router } from 'expo-router';
@@ -141,31 +141,3 @@ const styles = StyleSheet.create({
         fontSize: 12,
     },
 });
-// END of MODEL_A solution ------------------------------------------------------------------------------------------------------------------------
-
-// import { Link } from 'expo-router';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function ModalScreen() {
-//     return (
-//         <View style={styles.container}>
-//             <Text>This is a modal</Text>
-//             <Link href="/" dismissTo style={styles.link}>
-//                 <Text>Go to home screen</Text>
-//             </Link>
-//         </View>
-//     );
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//         padding: 20,
-//     },
-//     link: {
-//         marginTop: 15,
-//         paddingVertical: 15,
-//     },
-// });
